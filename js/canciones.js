c370b05b9fa38412f1dd7eda94c6fa271770f569
KevSchmitz <110779916+KevSchmitz@users.noreply.github.com>
2022-08-30 18:08:53 -0300

    Ya funciona el reproductor

--- js/canciones.js
+++ js/canciones.js
@@ -1,12 +1,12 @@
 const canciones = [
-  new Cancion('1', 'Titi me preguntó', 'Bad Bunny', 'Verano sin ti', 'verano sin ti', '4:50', 'Bad Bunny - Tití Me Preguntó (Official Video) - Un Verano Sin Ti.mp3'),
+  new Cancion('1', 'Titi me preguntó', 'Bad Bunny', 'Verano sin ti', 'verano sin ti', '4:50', 'Bad Bunny - Tití Me Preguntó (Official Video) - Un Verano Sin Ti'),
 
-  new Cancion('2', 'Yo no soy celoso', 'Bad Bunny', 'Verano sin ti', 'verano sin ti', '3:50', 'Bad Bunny - Yo No Soy Celoso (360° Visualizer) - Un Verano Sin Ti.mp3'),
+  new Cancion('2', 'Yo no soy celoso', 'Bad Bunny', 'Verano sin ti', 'verano sin ti', '3:50', 'Bad Bunny - Yo No Soy Celoso (360° Visualizer) - Un Verano Sin Ti'),
 
-  new Cancion('3', 'Yonaguni', 'Bad Bunny', 'Yonaguni', 'yonaguni', '3:28', 'Bad Bunny - Yonaguni (Video Oficial).mp3'),
+  new Cancion('3', 'Yonaguni', 'Bad Bunny', 'Yonaguni', 'yonaguni', '3:28', 'Bad Bunny - Yonaguni (Video Oficial)'),
 
-  new Cancion('4', 'Me porto bonito', 'Bad Bunny', 'Verano sin ti', 'verano sin ti', '3:11', 'Bad Bunny (ft. Chencho Corleone) - Me Porto Bonito (Official Video) - Un Verano Sin Ti.mp3'),
+  new Cancion('4', 'Me porto bonito', 'Bad Bunny', 'Verano sin ti', 'verano sin ti', '3:11', 'Bad Bunny (ft. Chencho Corleone) - Me Porto Bonito (Official Video) - Un Verano Sin Ti'),
 
-  new Cancion('5', 'Ojitos lindos', 'Bad Bunny', 'Verano sin ti', 'verano sin ti', '4:118', 'Bad Bunny, Bomba Estéreo - Ojitos Lindos (Letra-Lyrics).mp3')
+  new Cancion('5', 'Ojitos lindos', 'Bad Bunny', 'Verano sin ti', 'verano sin ti', '4:118', 'Bad Bunny, Bomba Estéreo - Ojitos Lindos (Letra-Lyrics)')
 ];
 
--- js/script.js
+++ js/script.js
@@ -1,17 +1,80 @@
 const cancionContenedor = document.querySelector('.cancion-contenedor');
-let nombreCancion = cancionContenedor.querySelector('.cancion-nombre a');
-let artistaCancion = cancionContenedor.querySelector('.cancion-artista a');
-let imagenCancion = cancionContenedor.querySelector('.cancion-album img');
+const nombreCancion = cancionContenedor.querySelector('.cancion-nombre a');
+const artistaCancion = cancionContenedor.querySelector('.cancion-artista a');
+const imagenCancion = cancionContenedor.querySelector('.cancion-album img');
+const audioPrincipal = cancionContenedor.querySelector('#audio-principal');
 
-const indiceArreglo = 0
+const reproductor = document.querySelector('reproductor');
+const botonPlayPause = document.querySelector('.play-pause img');
+const botonAtras = document.querySelector('#atras');
+const botonSiguiente = document.querySelector('#siguiente');
 
-nombreCancion.textContent = canciones[`${indiceArreglo}`].nombre;
-artistaCancion.textContent = canciones[`${indiceArreglo}`].artista;
-imagenCancion.src = 'img/' + canciones[`${indiceArreglo}`].imagen + '.jpg';
+
+let indiceArreglo = 1;
+
+window.addEventListener('load', () => {
+  cargarCancion(indiceArreglo);
+})
+
+function cargarCancion(numeroIndice) {
+  nombreCancion.textContent = canciones[`${numeroIndice}`].nombre;
+  artistaCancion.textContent = canciones[`${numeroIndice}`].artista;
+  imagenCancion.src = `img/${canciones[numeroIndice].imagen}.jpg`;
+  audioPrincipal.src = `songs/${canciones[numeroIndice].src}.mp3`;
+}
+
+function reproducirCancion() {
+  cancionContenedor.classList.add('pausado');
+  botonPlayPause.src = 'iconosweb/reproductor/pausa.png'
+  audioPrincipal.play();
+}
+function pausarCancion() {
+  cancionContenedor.classList.remove('pausado');
+  botonPlayPause.src = 'iconosweb/reproductor/jugar.png'
+  audioPrincipal.pause();
+}
+function atrasCancion() {
+
+  indiceArreglo--;
+  indiceArreglo < 0 ? indiceArreglo = canciones.length - 1 : indiceArreglo = indiceArreglo;
+  cargarCancion(indiceArreglo);
+  reproducirCancion();
+  console.log(indiceArreglo);
+
+}
+function adelantarCancion() {
+
+  indiceArreglo++;
+  indiceArreglo > canciones.length - 1 ? indiceArreglo = 0 : indiceArreglo = indiceArreglo;
+  cargarCancion(indiceArreglo);
+  reproducirCancion();
+  console.log(indiceArreglo);
+}
+
+
+botonPlayPause.addEventListener('click', () => {
+  const estaPausadoCancion = cancionContenedor.classList.contains('pausado');
+  // Si estaPausadoCancion es true activa función pausarCancion, sino activa función reproducirCancion.
+  estaPausadoCancion ? pausarCancion() : reproducirCancion();
+});
+
+botonAtras.addEventListener('click', () => {
+  atrasCancion()
+});
+botonSiguiente.addEventListener('click', () => {
+  adelantarCancion();
+});
 
 
 console.log(cancionContenedor);
 console.log(nombreCancion);
 console.log(artistaCancion);
 console.log(imagenCancion);
+console.log(audioPrincipal);
+console.log(botonPlayPause);
+console.log(botonAtras);
+console.log(botonSiguiente);
+console.log(canciones.length);
+console.log(indiceArreglo);
+
 
