6dbd5de64483924f6d64696e2aeb3874d0f9edf0
KevSchmitz <110779916+KevSchmitz@users.noreply.github.com>
2022-08-31 23:55:01 -0300

    Se agregó timer. Se agregó barra de progreso. Se agregó reproducción por click en barra de progreso.

--- js/functions.js
+++ js/functions.js
@@ -10,5 +10,38 @@ class Cancion {
   }
 }
 
-const agregarElemento = (arreglo, elemento) => arreglo.push(elemento);
 
+function cargarCancion(numeroIndice) {
+  nombreCancion.textContent = canciones[`${numeroIndice}`].nombre;
+  artistaCancion.textContent = canciones[`${numeroIndice}`].artista;
+  imagenCancion.src = `img/${canciones[numeroIndice].imagen}.jpg`;
+  audioPrincipal.src = `songs/${canciones[numeroIndice].src}.mp3`;
+}
+
+function reproducirCancion() {
+  cancionContenedor.classList.add('pausado');
+  botonPlayPause.src = 'iconosweb/reproductor/pausa.png'
+  audioPrincipal.play();
+}
+function pausarCancion() {
+  cancionContenedor.classList.remove('pausado');
+  botonPlayPause.src = 'iconosweb/reproductor/jugar.png'
+  audioPrincipal.pause();
+}
+function atrasCancion() {
+
+  indiceArreglo--;
+  indiceArreglo < 0 ? indiceArreglo = canciones.length - 1 : indiceArreglo = indiceArreglo;
+  cargarCancion(indiceArreglo);
+  reproducirCancion();
+  console.log(indiceArreglo);
+
+}
+function adelantarCancion() {
+
+  indiceArreglo++;
+  indiceArreglo > canciones.length - 1 ? indiceArreglo = 0 : indiceArreglo = indiceArreglo;
+  cargarCancion(indiceArreglo);
+  reproducirCancion();
+  console.log(indiceArreglo);
+}
--- js/script.js
+++ js/script.js
@@ -4,53 +4,61 @@ const artistaCancion = cancionContenedor.querySelector('.cancion-artista a');
 const imagenCancion = cancionContenedor.querySelector('.cancion-album img');
 const audioPrincipal = cancionContenedor.querySelector('#audio-principal');
 
-const reproductor = document.querySelector('reproductor');
-const botonPlayPause = document.querySelector('.play-pause img');
-const botonAtras = document.querySelector('#atras');
-const botonSiguiente = document.querySelector('#siguiente');
+const reproductor = document.querySelector('.reproductor');
+const botonPlayPause = reproductor.querySelector('.play-pause img');
+const botonAtras = reproductor.querySelector('#atras');
+const botonSiguiente = reproductor.querySelector('#siguiente');
 
+const barraTiempo = document.querySelector('.barra-tiempo');
+const barraProgreso = document.querySelector('.barra-progreso');
+
+
+// Progreso de del tiempo de la canción
+
+audioPrincipal.addEventListener('timeupdate', (e) => { // A medida que avanza el tiempo actualiza la salida
+  const tiempo = barraTiempo.querySelector('.tiempo');
+  const barraTiempoActual = e.target.currentTime;
+  const duracion = e.target.duration;
+  let barraProgreso = (barraTiempoActual / duracion) * 100;
+  tiempo.style.width = `${barraProgreso}%`
+
+  const tiempoActual = barraTiempo.querySelector('.tiempo-actual');
+  let minActual = Math.floor(audioPrincipal.currentTime / 60);
+  let secActual = Math.floor(audioPrincipal.currentTime % 60);
+
+  if (secActual < 10) {
+    secActual = `0${secActual}`;
+  }
+  tiempoActual.textContent = `${minActual}:${secActual}`;
+
+  audioPrincipal.addEventListener('loadeddata', () => { // Carga los datos primero para poder leerlos evita el NAN:NAN
+    const tiempoFaltante = barraTiempo.querySelector('.tiempo-faltante');
+    let minDuracion = Math.floor(audioPrincipal.duration / 60);
+    let secDuracion = Math.floor(audioPrincipal.duration % 60);
+    if (secDuracion < 10) {
+      secDuracion = `0${secDuracion}`;
+    }
+    tiempoFaltante.textContent = `${minDuracion}:${secDuracion}`;
+  })
+
+})
+
+barraProgreso.addEventListener('click', (e) => {
+  console.log(e);
+  let anchoBarra = barraProgreso.clientWidth; // Para obtener el ancho de la barra
+  console.log(anchoBarra);
+  let offSetXClick = e.offsetX; // Qué zona de la barra estoy tocando
+  console.log(offSetXClick);
+  let duracionCancion = audioPrincipal.duration;
+  console.log(duracionCancion);
+  audioPrincipal.currentTime = (offSetXClick / anchoBarra) * duracionCancion;
+})
 
 let indiceArreglo = 1;
 
 window.addEventListener('load', () => {
   cargarCancion(indiceArreglo);
 })
-
-function cargarCancion(numeroIndice) {
-  nombreCancion.textContent = canciones[`${numeroIndice}`].nombre;
-  artistaCancion.textContent = canciones[`${numeroIndice}`].artista;
-  imagenCancion.src = `img/${canciones[numeroIndice].imagen}.jpg`;
-  audioPrincipal.src = `songs/${canciones[numeroIndice].src}.mp3`;
-}
-
-function reproducirCancion() {
-  cancionContenedor.classList.add('pausado');
-  botonPlayPause.src = 'iconosweb/reproductor/pausa.png'
-  audioPrincipal.play();
-}
-function pausarCancion() {
-  cancionContenedor.classList.remove('pausado');
-  botonPlayPause.src = 'iconosweb/reproductor/jugar.png'
-  audioPrincipal.pause();
-}
-function atrasCancion() {
-
-  indiceArreglo--;
-  indiceArreglo < 0 ? indiceArreglo = canciones.length - 1 : indiceArreglo = indiceArreglo;
-  cargarCancion(indiceArreglo);
-  reproducirCancion();
-  console.log(indiceArreglo);
-
-}
-function adelantarCancion() {
-
-  indiceArreglo++;
-  indiceArreglo > canciones.length - 1 ? indiceArreglo = 0 : indiceArreglo = indiceArreglo;
-  cargarCancion(indiceArreglo);
-  reproducirCancion();
-  console.log(indiceArreglo);
-}
-
 
 botonPlayPause.addEventListener('click', () => {
   const estaPausadoCancion = cancionContenedor.classList.contains('pausado');
@@ -64,17 +72,3 @@ botonAtras.addEventListener('click', () => {
 botonSiguiente.addEventListener('click', () => {
   adelantarCancion();
 });
-
-
-console.log(cancionContenedor);
-console.log(nombreCancion);
-console.log(artistaCancion);
-console.log(imagenCancion);
-console.log(audioPrincipal);
-console.log(botonPlayPause);
-console.log(botonAtras);
-console.log(botonSiguiente);
-console.log(canciones.length);
-console.log(indiceArreglo);
-
-
